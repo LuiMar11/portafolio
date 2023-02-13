01ac10dbbe0ab138f89a67543ed571a012061718
LuiMar11 <64877820+LuiMar11@users.noreply.github.com>
2023-02-13 16:06:12 -0500

    spa

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+(function () {
+  var redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  if (redirect && redirect != location.href) {
+    history.replaceState(null, null, redirect);
+  }
+})();
+
 document.addEventListener(
   "DOMContentLoaded",
   () => {
